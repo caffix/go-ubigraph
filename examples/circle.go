73bc1e442985386e4c4556ff9f6ea1321e73cff0
Jeff <caffix@users.noreply.github.com>
2016-04-09 14:37:44 -0400

    Added files via upload
    Added an example of drawing a circle.

---
+++ examples/circle.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"caffix/ubigraph"
+	"fmt"
+	"time"
+)
+
+func callback(id int) {
+	fmt.Printf("Left Double-Click for Vertex: %d\n", id)
+}
+
+func main() {
+	graph, _ := ubigraph.NewClient()
+	graph.Clear()
+
+	sytleID, err := graph.NewVertexStyle(0)
+	if err != nil {
+		return
+	}
+	graph.SetVertexStyleAttribute(sytleID, "shape", "sphere")
+	graph.SetVertexStyleAttribute(sytleID, "color", "#ff0000")
+
+	go func() {
+		cirlen := 10
+		vertices := make([]int, cirlen, cirlen)
+		for i := 0; i < cirlen; i = i + 1 {
+			vertices[i], _ = graph.NewVertex()
+			graph.ChangeVertexStyle(vertices[i], sytleID)
+		}
+
+		for i := 0; i < cirlen; i = i + 1 {
+			time.Sleep(time.Second)
+			if i != 0 {
+				graph.NewEdge(vertices[i-1], vertices[i])
+			}
+			graph.NewEdge(vertices[i], vertices[(i+1)%cirlen])
+		}
+	}()
+	graph.SetCallbackServerAddr("127.0.0.1")
+	graph.SetCallbackServerPort(20740)
+	graph.SetCallbackRoutine(callback)
+	graph.SetVertexStyleCallback(sytleID)
+	graph.StartCallbackServer()
+}
